aa135b60870530e2ad2ae60d058f1dc8970281ce
siphoPrince <mabirimisesipho@gmail.com>
2023-06-05 11:43:21 -0700

    combining strings

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "welcome to"
-str2 = " Holberton School"
-# str1 += " " + str2
-print(f"{str1}{str2}")
-
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
