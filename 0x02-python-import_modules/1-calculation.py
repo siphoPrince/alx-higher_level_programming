cbd41569c683da4ec421f34b9d0cf7f7ef7ed24b
siphoPrince <mabirimisesipho@gmail.com>
2023-06-08 10:29:24 -0700

    making calculations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
 
-a = 10
-b = 5
-
-sumof = add(a, b)
-print("{} + {} = {}".format(a,b,sumof))
-
-divof = div(a, b)
-print("{} / {} = {}".format(a,b,divof))
-
-subof = sub(a, b)
-print("{} - {} = {}".format(a,b,subof))
-
-multof = mul(a, b)
-print("{} * {} = {}".format(a,b, multof))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
