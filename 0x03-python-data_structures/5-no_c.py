25b900f4e3335ce5f0db630f041db39495c7e009
siphoPrince <mabirimisesipho@gmail.com>
2023-06-12 11:17:16 -0700

    deleting letters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = " "
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-            return new_string
+    strcp = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(strcp))
