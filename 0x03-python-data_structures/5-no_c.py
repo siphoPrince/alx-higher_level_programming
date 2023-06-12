3367f2afdd9d6d46f98336a6744b2cd7914015f0
siphoPrince <mabirimisesipho@gmail.com>
2023-06-12 07:24:18 -0700

    taking out the cs

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = " "
+    for char in new_string:
+        if char != 'C' and char != 'c':
+            new_string += char
+            return new_string
