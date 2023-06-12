d3994110322cb3c3fce1902cac470130403b3850
siphoPrince <mabirimisesipho@gmail.com>
2023-06-12 09:07:41 -0700

    adding switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-a, b = 89, 10
-a = b
-b = a
-print("a={:d} - b={:d}".format(a, b))
+a = 89
+b = 10
+# Your code here (inserted at line 4)
+a, b = b, a
+print("a =", a, "- b =", b)
