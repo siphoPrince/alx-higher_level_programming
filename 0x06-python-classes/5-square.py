a55d7dfb6ff8aa3164664618ce3f0605eb8b63f2
siphoPrince <mabirimisesipho@gmail.com>
2023-06-27 09:53:11 -0700

    adding

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define a class Square."""
-class Square:
+    class Square:
     """Represent a square."""
 
     def __init__(self, size):
@@ -13,7 +13,7 @@ class Square:
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """set the current size of the square."""
         return (self.__size)
 
     @size.setter
