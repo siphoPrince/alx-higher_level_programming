15c971edd65a084d3e05c87395d8f51f86cd0cad
siphoPrince <mabirimisesipho@gmail.com>
2023-07-12 08:44:04 -0700

    adding to string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a JSON function."""
+"""Defines a string to JSON function."""
 import json
 
 
 def to_json_string(my_obj):
-    """Return a string object."""
+    """Return the JSON string object."""
     return json.dumps(my_obj)
