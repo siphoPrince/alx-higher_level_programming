3aaea6ffd22e7318b45d20a215555a58f2ebb70f
siphoPrince <mabirimisesipho@gmail.com>
2023-11-13 11:19:24 -0800

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const arg = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(arg));
