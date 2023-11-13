92273013fc0151c7172a578a3bd75991e93b3a90
siphoPrince <mabirimisesipho@gmail.com>
2023-11-13 07:26:19 -0800

    fixing semi

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,13 +5,13 @@ const sizeArg = process.argv[2];
 const size = parseInt(sizeArg);
 
 if (!isNaN(size) && isFinite(size) && size > 0) {
-    for (let i = 0; i < size; i++) {
-        let row = '';
-        for (let j = 0; j < size; j++) {
-            row += 'X';
-        }
-        console.log(row);
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
     }
+    console.log(row);
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
