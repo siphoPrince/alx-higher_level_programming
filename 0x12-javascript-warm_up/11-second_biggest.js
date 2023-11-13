6db8b5b4aa776f4c7ef723ee7e7a260ce07f1757
siphoPrince <mabirimisesipho@gmail.com>
2023-11-13 12:11:28 -0800

    printing biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const sortedIntegers = args.map(Number).sort((a, b) => b - a);
+
+  console.log(sortedIntegers[1]);
+}
