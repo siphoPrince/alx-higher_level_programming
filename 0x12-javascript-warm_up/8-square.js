37198865098c2dc535aaa4fd6ca517a462e76832
siphoPrince <mabirimisesipho@gmail.com>
2023-11-13 07:08:49 -0800

    printing squares

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const myArgs = process.argv[2];
+
+const argConvert = parseInt(myArgs);
+
+if (!isNaN(argConvert)) {
+  for (let i = 0; i < argConvert; i++) {
+    let row = '';
+    for (let j = 0; j < argConvert; j++) {
+      row += 'X ';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
