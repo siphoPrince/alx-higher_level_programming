c8d42bd981439097538dc4d43e0a44df02cca689
siphoPrince <mabirimisesipho@gmail.com>
2023-11-13 06:09:00 -0800

    error fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,13 +2,12 @@
 
 const x = process.argv[2];
 
-const stringConvet = parseInt(x);
+const stringConvert = parseInt(x);
 
-if (!isNaN(stringConvet)) {
-  for (let i = 0; i < stringConvet; i++) {
-    console.log('c is fun');
+if (!isNaN(stringConvert) && isFinite(stringConvert)) {
+  for (let i = 0; i < stringConvert; i++) {
+    console.log('C is fun');
   }
 } else {
-      console.log('Missing number of occurrences');
-  }
-
+  console.log('Missing number of occurrences');
+}
