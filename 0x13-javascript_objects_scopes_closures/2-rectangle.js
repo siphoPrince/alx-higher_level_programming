87651781d7e00add7983fdfeed1d664cbb72c2bb
siphoPrince <mabirimisesipho@gmail.com>
2023-11-14 05:21:04 -0800

    printing rectangler class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return Object.create(null);
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || h === undefined) {
+      Object.create(null);
     } else {
       this.width = w;
       this.height = h;
     }
   }
 }
-
 
 module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0) {
       return Object.create(null);
     } else {
@@ -10,17 +10,17 @@ class Rectangle {
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
