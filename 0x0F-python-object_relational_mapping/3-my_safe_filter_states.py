dee44caa6026ab9f1efa9920a1967531fff5ff19
siphoPrince <mabirimisesipho@gmail.com>
2023-11-17 04:14:55 -0800

    cities view

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that takes in arguments and displays
+all values in the states table 
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            password=argv[2],
+            db=argv[3]
+            )
+    cur = conn.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN " \
+            "states ON cities.state_id = states.id"
+
+    if cur.execute(query):
+        rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    conn.close()
+    cur.close()
