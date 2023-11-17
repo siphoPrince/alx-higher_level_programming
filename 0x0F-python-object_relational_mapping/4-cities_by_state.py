d883273328e7f0b6c68bb98793215343ba9bbaee
siphoPrince <mabirimisesipho@gmail.com>
2023-11-17 04:46:25 -0800

    query selector fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,20 +8,15 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            password=argv[2],
-            db=argv[3]
-            )
+
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+
     cur = conn.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
-
-    rows = cur.fetchall()
-
-    for row in rows:
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
+                "states ON cities.state_id = states.id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    conn.close()
     cur.close()
+    conn.close()
