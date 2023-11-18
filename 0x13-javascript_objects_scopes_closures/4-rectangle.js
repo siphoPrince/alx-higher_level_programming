28d55296d0f2377536f265ca28fd32ef55a18ab0
siphoPrince <mabirimisesipho@gmail.com>
2023-11-18 08:23:16 -0800

    defining a rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return Object.create(null);
-    } else {
+    if ((w = parseInt(w)) > 0 &&
+            (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
+    for (let index = 0; index < this.height; index++) {
       console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const rotatedValues = this.width;
+    this.width = this.height;
+    this.height = rotatedValues;
   }
 
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-}
-
-module.exports = Rectangle;
+};
