4c415f652310489ccee1c75c9f6140ad04f0ade7
siphoPrince <mabirimisesipho@gmail.com>
2023-11-18 03:05:54 -0800

    state filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """
-script that takes in arguments and displays
-all values in the states table
+ write a script that takes in arguments and
+ displays all values in the states
+
 """
-
-
 import MySQLdb
 from sys import argv
 
+
+def name_state():
+    """list states"""
+    database = MySQLdb.connect(host="localhost",
+                               user=argv[1],
+                               passwd=argv[2],
+                               db=argv[3],
+                               port=3306)
+    c = database.cursor()
+
+    name_search = argv[4]
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC"
+    c.execute(query, (f'%{name_search}%',))
+
+    row = c.fetchall()
+
+    for rows in row:
+        print(rows)
+
+    c.close()
+    database.close()
+
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            password=argv[2],
-            db=argv[3]
-            )
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
-                    (argv[4], ))
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    conn.close()
-    cur.close()
+    name_state()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+Class definition of a State and an instance Base = declarative_base()
 """
 
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+# Replace 'username', 'password', 'database' with your actual MySQL connection details
+DATABASE_URL = "mysql+mysqlconnector://username:password@localhost:3306/database"
 
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(DATABASE_URL, echo=True)
     Base.metadata.create_all(engine)
+
