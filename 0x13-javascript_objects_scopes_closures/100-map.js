48600127f69b52584984526ead7b28f8ff86a0d3
siphoPrince <mabirimisesipho@gmail.com>
2023-11-18 08:25:55 -0800

    prinitng the maps

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-const data = require('./100-data');
+const factor = require('./100-data').list;
 
-const initialList = data.list;
-
-const newList = initialList.map((value, index) => value * index);
-
-console.log(initialList);
-console.log(newList);
+const map1 = factor.map((index, list1) => index * list1);
+console.log(factor);
+console.log(map1);
