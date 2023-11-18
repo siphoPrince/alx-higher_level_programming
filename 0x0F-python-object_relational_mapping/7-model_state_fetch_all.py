9fbfdbb5d196494938d7ac511c237411e99d1d9f
siphoPrince <mabirimisesipho@gmail.com>
2023-11-18 03:24:29 -0800

    fixed error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""lists all State objects
-    from the database hbtn_0e_6_usa"""
-import sys
-import sqlalchemy
-from model_state import Base, State
+'''
+lists all State objects from the database hbtn_0e_6_usa
+'''
+import MySQLdb
+from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
-
-def all_state(State):
-    """lists all states"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database_name), pool_pre_ping=True)
-    s = Session(engine)
-    for i in s.query(State).order_by(State.id).all():
-        print(f"{i.id}: {i.name}")
-    s.close()
-
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    all_state(State)
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print('{}: {}'.format(state.id, state.name))
+
+    session.close()
