cfa805b95eccafc130f4c0df01f939e7b5b1f9be
siphoPrince <mabirimisesipho@gmail.com>
2023-11-18 08:20:30 -0800

    printing rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w >= 0 || h >= 0 || h === undefined) {
-      Object.create(null);
-    } else {
+    if ((w = parseInt(w)) > 0 &&
+        (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
     }
   }
-}
-
-module.exports = Rectangle;
+};
