8ef1885caddb6d3bfd8b9df15adcb7f62f312b2b
siphoPrince <mabirimisesipho@gmail.com>
2023-12-01 04:23:40 -0800

    sending body via url

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.error
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ./3-error_code.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode('utf-8')
+        print(content)
+
+except urllib.error.HTTPError as e:
+    print("Error code:", e.code)
