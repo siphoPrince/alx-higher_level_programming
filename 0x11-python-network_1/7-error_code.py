42e915980b448582f64b47edda49b86d6a69101e
siphoPrince <mabirimisesipho@gmail.com>
2023-12-01 04:38:22 -0800

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ./7-error_code.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+response = requests.get(url)
+
+if response.status_code >= 400:
+    print("Error code:", response.status_code)
+else:
+    print(response.text)
