1fe3493be2c256558294e803f65630f41039e651
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 09:47:28 -0800

    code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
-const request = require('request');
 
-request(process.argv[2].pipe(fs.createWriteStream(process.argv[3])));
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+const fileStream = fs.createWriteStream(fileName);
+request(url).pipe(fileStream);
