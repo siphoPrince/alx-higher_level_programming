d3691b49e0cd22f10e982c3b0b094cf8e3c9adca
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 09:43:41 -0800

    lorome

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,4 +2,4 @@
 const fs = require('fs');
 const request = require('request');
 
-request(process.argv[2].pipe(fs.createWriteStream(process.argv[3]));
+request(process.argv[2].pipe(fs.createWriteStream(process.argv[3])));
