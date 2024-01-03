0defadb16336292e93d4a2bfef049e5fac5bb3f7
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 04:56:11 -0800

    starwars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    try {
+      const movieData = JSON.parse(body);
+      console.log(`${movieData.title}`);
+    } catch (parseError) {
+      console.error(`Error parsing JSON: ${parseError}`);
+    }
+  }
+});
