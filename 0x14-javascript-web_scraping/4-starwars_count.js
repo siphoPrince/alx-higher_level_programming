2f03f27337e0950e6e8ae77893e3487f88131e70
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 09:49:43 -0800

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,18 @@
 #!/usr/bin/node
-
 const request = require('request');
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-// Character ID for Wedge Antilles
-const wedgeAntillesId = 18;
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    try {
-      const filmsData = JSON.parse(body).results;
-
-      // Filter films where Wedge Antilles is present
-      const filmsWithWedgeAntilles = filmsData.filter((film) =>
-        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)
-      );
-
-      console.log(`${filmsWithWedgeAntilles.length}`);
-    } catch (parseError) {
-      console.error(`Error parsing JSON: ${parseError}`);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  let count = 0;
+  for (const result of JSON.parse(body).results) {
+    for (const wedgeUrl of result.characters) {
+      if (wedgeUrl.includes(18)) {
+        count++;
+      }
     }
   }
+  console.log(count);
 });
