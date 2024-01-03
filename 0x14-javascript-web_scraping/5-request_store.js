cb6513a12513d3109c158083bdc251ac3ee59c55
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 09:40:36 -0800

    pipe add

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-const url = process.argv[2];
 
-request(url.pipe(fs.createWriteStream(process.argv[3]));
+request(process.argv[2].pipe(fs.createWriteStream(process.argv[3]));
