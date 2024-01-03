657955359ee5482c343c989113e1924ee36f9671
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 04:33:44 -0800

    error printing

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filename = process.argv[2];
-const content = process.argv[3];
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
 
-fs.writeFile(filename, content, 'utf-8', (error) => {
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
   if (error) {
     console.log(error);
+  } else {
+    console.log(contentToWrite);
   }
 });
