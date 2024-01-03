6e73b99c962c6583d0d3677f58535724594652fd
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 09:31:57 -0800

    requeststore

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,30 +1,22 @@
 #!/usr/bin/node
 
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get({ url, encoding: 'utf-8' }, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+axios.get(url)
+  .then(response => {
+    const content = response.data;
+    fs.writeFile(filePath, content, 'utf-8', (writeError) => {
       if (writeError) {
         console.error(writeError);
       } else {
         console.log(`Webpage content saved to: ${filePath}`);
-        
-        // Read and print the contents of the saved file
-        fs.readFile(filePath, 'utf-8', (readError, fileContent) => {
-          if (readError) {
-            console.error(readError);
-          } else {
-            console.log(fileContent.trim());  // Trim to remove leading/trailing whitespaces
-          }
-        });
       }
     });
-  }
-});
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
